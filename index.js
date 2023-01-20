91bc2f846f3992ba2a37a215efd5bc1499425a13
Hamad Bakeel <90466313+HamadBakeel@users.noreply.github.com>
2023-01-21 00:53:05 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(array, name){
+    const messagesArray = [];
+    for (let i = 0; i < array.length; i++) {
+        messagesArray[i] = `Thank you, ${array[i]}, for the wonderful ${name} gift!`
+    }
+
+    return messagesArray;
+}
+
+function countDown(num){
+    for (let i = num; i >= 0 ; i--) {
+        console.log(i)
+    }
+}
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
